19e8a1798fd3e0410d140c921bce8a04f5f1dabf
Kaviya <kaviya29090@gmail.com>
2025-11-26 21:20:55 +0530

    final commit

--- public/phone.js
+++ public/phone.js
@@ -2,14 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const links = document.querySelectorAll('[href="#phone-link"]');
 
   links.forEach((link) => {
-    if (window.PHONE_NUMBER) {
-      // Set the actual tel link
-      link.setAttribute("href", `tel:${window.PHONE_NUMBER}`);
-
-      // Optional: If the link has no visible text, you can insert the number
-      if (!link.textContent.trim()) {
-        link.textContent = window.PHONE_NUMBER;
-      }
-    }
+    link.setAttribute("href", `tel:${process.env.REACT_APP_PHONE_NUMBER}`);
+    if (!link.textContent.trim()) link.textContent = process.env.REACT_APP_PHONE_NUMBER;
   });
 });
